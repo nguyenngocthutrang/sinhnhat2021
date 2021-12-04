a286ebbcd2f420a798384803e7aa41147d1f309c
nguyenngocthutrang <95540551+nguyenngocthutrang@users.noreply.github.com>
2021-12-05 00:18:56 +0700

    add gift

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em gái';
+const giftUrl = 'https://nguyenngocthutrang.github.io/quasinhnhat22tuoi/';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng em gái cute';
+const contentCard = 'Happy birthday tuổi 22 !!!!! Chúc em gái tuổi mới xinh gái hơn, chăm hơn, giàu hơn và luôn tỏa sáng em nhé 💘💘💘💘💘💘';
+
+const giftImage1 = 'hot-girl.png';
+// const giftImage2 = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
