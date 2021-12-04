002fe032b3902871dd241e8904a838d8c9736328
nguyenngocthutrang <95540551+nguyenngocthutrang@users.noreply.github.com>
2021-12-05 00:25:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ const giftUrl = 'https://nguyenngocthutrang.github.io/sinhnhat2021/';
 const eventName = 'Chúc Mừng Sinh Nhật';
 const titleCard = 'Tặng em gái cute';
 const contentCard = 'Happy birthday tuổi 22 !!!!! Chúc em gái tuổi mới xinh gái hơn, chăm hơn, giàu hơn và luôn tỏa sáng em nhé 💘💘💘💘💘💘';
-
 const giftImage1 = 'hot-girl.png';
 // const giftImage2 = 'hot-girl.png';
 const base64 = '';
